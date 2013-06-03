e39a437dcd2ebceec193427122050d068f846466
jevans <john.g.evans.ne@gmail.com>
2013-06-02 21:26:29 -0400

    Bumping to 0.1.3p1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 kwargs = {'name': 'Glymur',
-          'version': '0.1.3',
+          'version': '0.1.3p1',
           'description': 'Tools for accessing JPEG2000 files',
           'long_description': open('README.md').read(),
           'author': 'John Evans',
