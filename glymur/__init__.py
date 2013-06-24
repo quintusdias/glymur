6eafc2ad3dc02b9586bee78c5de84d289d70b3c2
John Evans <john.g.evans.ne@gmail.com>
2013-06-24 08:54:08 -0400

    Added runtests function to discover and run tests from within python.

--- glymur/__init__.py
+++ glymur/__init__.py
@@ -1,9 +1,15 @@
 """glymur - read, write, and interrogate JPEG 2000 files
 """
 
-
 from .jp2k import Jp2k
 from .jp2dump import jp2dump
 
 from . import data
 from . import test
+
+def runtests():
+    """Discover and run all tests for the glymur package.
+    """
+    import unittest
+    suite = unittest.defaultTestLoader.discover(__path__[0])
+    unittest.TextTestRunner(verbosity=2).run(suite)
