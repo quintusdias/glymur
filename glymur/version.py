f551646af8072037179e12cf1846c7f6ba7f8ad1
John Evans <john.g.evans.ne@gmail.com>
2013-10-29 10:15:24 -0400

    Prepping 0.5.9 release.

--- glymur/version.py
+++ glymur/version.py
@@ -15,7 +15,7 @@ from .lib import openjp2 as opj2
 
 # Do not change the format of this next line!  Doing so risks breaking
 # setup.py
-version = "0.5.8"
+version = "0.5.9"
 _sv = LooseVersion(version)
 version_tuple = _sv.version
 
