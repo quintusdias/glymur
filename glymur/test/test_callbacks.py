da25953de30ab2864fc413cb26dc569a195c3eb8
jevans <john.g.evans.ne@gmail.com>
2014-10-21 19:43:26 -0400

    updated tests to run on fully loaded platform, closes #287

--- glymur/test/test_callbacks.py
+++ glymur/test/test_callbacks.py
@@ -25,8 +25,6 @@ import glymur
 
 from .fixtures import WARNING_INFRASTRUCTURE_ISSUE, WARNING_INFRASTRUCTURE_MSG
 
-@unittest.skipIf(glymur.lib.openjp2.OPENJP2 is None,
-                 "Missing openjp2 library.")
 class TestCallbacks(unittest.TestCase):
     """Test suite for callbacks."""
 
@@ -37,6 +35,8 @@ class TestCallbacks(unittest.TestCase):
     def tearDown(self):
         pass
 
+    @unittest.skipIf(glymur.version.openjpeg_version[0] != '2',
+                     "Missing openjp2 library.")
     @unittest.skipIf(WARNING_INFRASTRUCTURE_ISSUE, WARNING_INFRASTRUCTURE_MSG)
     @unittest.skipIf(os.name == "nt", "Temporary file issue on window.")
     def test_info_callback_on_write(self):
@@ -59,46 +59,20 @@ class TestCallbacks(unittest.TestCase):
         # callback handler is enabled.
         j = glymur.Jp2k(self.j2kfile)
         with patch('sys.stdout', new=StringIO()) as fake_out:
-            j.read(rlevel=1, verbose=True, area=(0, 0, 200, 150))
+            j.read(rlevel=1, verbose=True)
             actual = fake_out.getvalue().strip()
 
-        lines = ['[INFO] Start to read j2k main header (0).',
-                 '[INFO] Main header has been correctly decoded.',
-                 '[INFO] Setting decoding area to 0,0,150,200',
-                 '[INFO] Header of tile 0 / 0 has been read.',
-                 '[INFO] Tile 1/1 has been decoded.',
-                 '[INFO] Image data has been updated with tile 1.']
+        if glymur.version.openjpeg_version[0] == '2':
+            lines = ['[INFO] Start to read j2k main header (0).',
+                     '[INFO] Main header has been correctly decoded.',
+                     '[INFO] No decoded area parameters, set the decoded area to the whole image',
+                     '[INFO] Header of tile 0 / 0 has been read.',
+                     '[INFO] Tile 1/1 has been decoded.',
+                     '[INFO] Image data has been updated with tile 1.']
 
-        expected = '\n'.join(lines)
-        self.assertEqual(actual, expected)
-
-
-@unittest.skipIf(glymur.lib.openjpeg.OPENJPEG is None,
-                 "Missing openjpeg library.")
-class TestCallbacks15(unittest.TestCase):
-    """This test suite is for OpenJPEG 1.5.1 properties.
-    """
-
-    def setUp(self):
-        self.jp2file = glymur.data.nemo()
-        self.j2kfile = glymur.data.goodstuff()
-
-    def tearDown(self):
-        pass
-
-    def test_info_callbacks_on_read(self):
-        """Verify stdout when reading.
-
-        Verify that we get the expected stdio output when our internal info
-        callback handler is enabled.
-        """
-        with patch('glymur.lib.openjp2.OPENJP2', new=None):
-            # Force to use OPENJPEG instead of OPENJP2.
-            j = glymur.Jp2k(self.j2kfile)
-            with patch('sys.stdout', new=StringIO()) as fake_out:
-                j.read(rlevel=1, verbose=True)
-                actual = fake_out.getvalue().strip()
-
+            expected = '\n'.join(lines)
+            self.assertEqual(actual, expected)
+        else:
             regex = re.compile(r"""\[INFO\]\stile\s1\sof\s1\s+
                                    \[INFO\]\s-\stiers-1\stook\s
                                            [0-9]+\.[0-9]+\ss\s+
@@ -114,7 +88,3 @@ class TestCallbacks15(unittest.TestCase):
                 self.assertRegexpMatches(actual, regex)
             else:
                 self.assertRegex(actual, regex)
-
-
-if __name__ == "__main__":
-    unittest.main()
