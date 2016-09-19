ac9c3ee33a04776700a505e1a9f964dc994e1dcf
John Evans <john.g.evans.ne@gmail.com>
2016-09-19 18:56:06 -0400

    Release v0.8.6

--- glymur/version.py
+++ glymur/version.py
@@ -22,7 +22,7 @@ from .lib import openjpeg as opj, openjp2 as opj2
 
 # Do not change the format of this next line!  Doing so risks breaking
 # setup.py
-version = "0.8.6rc1"
+version = "0.8.6"
 _sv = LooseVersion(version)
 version_tuple = _sv.version
 
