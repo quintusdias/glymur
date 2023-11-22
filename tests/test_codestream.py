d9713999b86fb9f5b0efe60e6f98f4a40f5e83d4
John Evans <john.evans8@montana.edu>
2023-11-22 14:08:46 -0700

    Refactor codestream tests
    No need for three test suites.

--- tests/test_codestream.py
+++ tests/test_codestream.py
@@ -16,10 +16,12 @@ from glymur import Jp2k
 from . import fixtures
 
 
-class TestSuite(unittest.TestCase):
+class TestSuite(fixtures.TestCommon):
     """Test suite for ICC Profile code."""
 
     def setUp(self):
+        super().setUp()
+
         self.p0_03 = ir.files('tests.data').joinpath('p0_03.j2k')
         self.p0_06 = ir.files('tests.data').joinpath('p0_06.j2k')
         self.p1_06 = ir.files('tests.data').joinpath('p1_06.j2k')
@@ -72,6 +74,7 @@ class TestSuite(unittest.TestCase):
             # Lots of things wrong with this file.
             warnings.simplefilter('ignore')
             jp2 = Jp2k(self.edf_c2_1178956)
+
         c = jp2.get_codestream()
         self.assertEqual(c.segment[2].zppm, 0)
         self.assertEqual(len(c.segment[2].data), 9)
@@ -96,6 +99,45 @@ class TestSuite(unittest.TestCase):
         self.assertEqual(c.segment[-1].crgn, 0)
         self.assertEqual(c.segment[-1].srgn, 0)
         self.assertEqual(c.segment[-1].sprgn, 11)
+
+    def test_reserved_marker_segment(self):
+        """
+        SCENARIO:  Rewrite a J2K file to include a marker segment with a
+        reserved marker 0xff6f (65391).
+
+        EXPECTED RESULT:  The marker segment should be properly parsed.
+        """
+
+        with open(self.temp_j2k_filename, 'wb') as tfile:
+            with open(self.j2kfile, 'rb') as ifile:
+                # Everything up until the first QCD marker.
+                read_buffer = ifile.read(65)
+                tfile.write(read_buffer)
+
+                # Write the new marker segment, 0xff6f = 65391
+                read_buffer = struct.pack('>HHB', int(65391), int(3), int(0))
+                tfile.write(read_buffer)
+
+                # Get the rest of the input file.
+                read_buffer = ifile.read()
+                tfile.write(read_buffer)
+                tfile.flush()
+
+        codestream = Jp2k(tfile.name).get_codestream()
+
+        self.assertEqual(codestream.segment[3].marker_id, '0xff6f')
+        self.assertEqual(codestream.segment[3].length, 3)
+        self.assertEqual(codestream.segment[3].data, b'\x00')
+
+    def test_siz_segment_ssiz_unsigned(self):
+        """ssiz attribute to be removed in future release"""
+        j = Jp2k(self.jp2file)
+        codestream = j.get_codestream()
+
+        # The ssiz attribute was simply a tuple of raw bytes.
+        # The first 7 bits are interpreted as the bitdepth, the MSB determines
+        # whether or not it is signed.
+        self.assertEqual(codestream.segment[1].ssiz, (7, 7, 7))
 
 
 class TestCodestreamRepr(unittest.TestCase):
@@ -141,46 +183,3 @@ class TestCodestreamRepr(unittest.TestCase):
         self.assertEqual(newseg.yrsiz, (1, 1, 1))
         self.assertEqual(newseg.bitdepth, (8, 8, 8))
         self.assertEqual(newseg.signed, (False, False, False))
-
-
-class TestCodestream(fixtures.TestCommon):
-    """Test suite for unusual codestream cases."""
-
-    def test_reserved_marker_segment(self):
-        """
-        SCENARIO:  Rewrite a J2K file to include a marker segment with a
-        reserved marker 0xff6f (65391).
-
-        EXPECTED RESULT:  The marker segment should be properly parsed.
-        """
-
-        with open(self.temp_j2k_filename, 'wb') as tfile:
-            with open(self.j2kfile, 'rb') as ifile:
-                # Everything up until the first QCD marker.
-                read_buffer = ifile.read(65)
-                tfile.write(read_buffer)
-
-                # Write the new marker segment, 0xff6f = 65391
-                read_buffer = struct.pack('>HHB', int(65391), int(3), int(0))
-                tfile.write(read_buffer)
-
-                # Get the rest of the input file.
-                read_buffer = ifile.read()
-                tfile.write(read_buffer)
-                tfile.flush()
-
-        codestream = Jp2k(tfile.name).get_codestream()
-
-        self.assertEqual(codestream.segment[3].marker_id, '0xff6f')
-        self.assertEqual(codestream.segment[3].length, 3)
-        self.assertEqual(codestream.segment[3].data, b'\x00')
-
-    def test_siz_segment_ssiz_unsigned(self):
-        """ssiz attribute to be removed in future release"""
-        j = Jp2k(self.jp2file)
-        codestream = j.get_codestream()
-
-        # The ssiz attribute was simply a tuple of raw bytes.
-        # The first 7 bits are interpreted as the bitdepth, the MSB determines
-        # whether or not it is signed.
-        self.assertEqual(codestream.segment[1].ssiz, (7, 7, 7))
