48d61c8c2375f26eebd99b466d789a0a045a9ea3
John Evans <john.evans8@montana.edu>
2024-05-07 13:19:34 -0600

    Release v0.13.2.post1

--- glymur/version.py
+++ glymur/version.py
@@ -20,7 +20,7 @@ from .lib import tiff
 
 # Do not change the format of this next line!  Doing so risks breaking
 # setup.py
-version = "0.13.2"
+version = "0.13.2.post1"
 
 version_tuple = parse(version).release
 
