4d6b0d1b2d28c1a7c70b1c523a8d04bef513c25b
John Evans <john.g.evans.ne@gmail.com>
2025-04-13 13:26:13 -0600

    Make tiff module in glymur.lib private
    I want the option of replacing the ctypes interface to libtiff sometime
    in the future.

--- glymur/lib/tiff.py
+++ glymur/lib/tiff.py
@@ -1,1 +1,10 @@
+"""
+This module is deprecated.  No external use, please.
+"""
+# standard library imports
+import warnings
+
+msg = "glymur.lib.tiff is for internal use only.  Please do not use it."
+warnings.warn(msg, DeprecationWarning)
 from ._tiff import *
+
