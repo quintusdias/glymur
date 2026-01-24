5c43cdd2b3dcd82496d7d246c1fe0eb8f8757bff
John Evans <john.g.evans.ne@gmail.com>
2026-01-24 14:44:46 -0700

    Remove two debugging breakpoints

--- glymur/jp2kr.py
+++ glymur/jp2kr.py
@@ -395,7 +395,6 @@ class Jp2kr(Jp2kBox):
         """
         .. deprecated:: 0.15.0
         """
-        breakpoint()
         msg = "Deprecated, do not use."
         warnings.warn(msg, DeprecationWarning)
         self._parse(force=force)
